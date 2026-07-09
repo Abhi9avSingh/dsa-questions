0d7e82af9e1c247d69ea96b710d7620717fde118
abhinav singh <abhinavsingh762006@gmail.com>
2026-07-09 15:12:02 +0530

    newcommit

--- nextGreaterElement.cpp
+++ nextGreaterElement.cpp
@@ -1,5 +1,12 @@
   
-        for (int i = 0; i <= nums1.size()-1 ; i++) {
+        
+ #include<bits/stdc++.h>
+
+ using namespace std;
+    int main (){
+        int nums1,nums2=0
+
+        for (int i = 0; i <=   nums1.size()-1 ; i++) {
             int flag = 0;
             int a = find(nums2.begin(), nums2.end(), nums1[i]) - nums2.begin();
             for (int j = a ; j < nums2.size()  ; j++) {
