d6ae8d76a9eaf2fb35601a6849298eb43720460b
abhinav singh <abhinavsingh762006@gmail.com>
2026-07-09 15:05:53 +0530

    newcommit

---
+++ nextGreaterElement.cpp
@@ -0,0 +1,17 @@
+  
+        for (int i = 0; i <= nums1.size()-1 ; i++) {
+            int flag = 0;
+            int a = find(nums2.begin(), nums2.end(), nums1[i]) - nums2.begin();
+            for (int j = a ; j < nums2.size()  ; j++) {
+                if (nums2[j] > nums1[i]) {
+                    ans.push_back(nums2[j]);
+                    flag++;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                ans.push_back(-1);
+            }
+        }
+         return ans;
+    }
