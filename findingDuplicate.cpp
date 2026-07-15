baa09aed95e7eb7f7d74c084486bf5911aad5f52
abhinav singh <abhinavsingh762006@gmail.com>
2026-07-15 22:48:09 +0530

    finding duplicate

--- findingDuplicate.cpp
+++ findingDuplicate.cpp
@@ -14,7 +14,19 @@ int findingDuplicate(vector<int>arr){
     }
     return ans;
 }
-
+// for multiple duplicate
+vector<int> findDuplicates(vector<int>& nums) {
+         
+     vector<int>ans;
+     sort(nums.begin(),nums.end());
+     for ( int i =0; i<nums.size()-1;i++){
+        if (nums[i]==nums[i+1]){
+            ans.push_back(nums[i]);
+            i++;
+        }
+     }
+     return ans;
+     }
 int main (){
     vector<int>arr={1,2,4,5,3,5};
    cout<< " "<< findingDuplicate(arr);
