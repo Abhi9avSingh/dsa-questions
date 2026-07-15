6e766884c0b93597b78be709895c56e501b36177
abhinav singh <abhinavsingh762006@gmail.com>
2026-07-15 21:59:39 +0530

    finding duplicate

---
+++ findingDuplicate.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int findingDuplicate(vector<int>arr){
+    
+    int ans=0;
+    for ( int i=0;i< arr.size();i++){
+        ans = ans^arr[i];
+    }
+     // Step 2: XOR numbers from 1 to n-1
+    for (int i = 1; i < arr.size(); i++) {
+        ans ^= i;
+    }
+    return ans;
+}
+
+int main (){
+    vector<int>arr={1,2,4,5,3,5};
+   cout<< " "<< findingDuplicate(arr);
+}
