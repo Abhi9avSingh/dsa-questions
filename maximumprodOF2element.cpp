87406ab433d750736ce3deda50edd61f165a8ef1
abhinav singh <abhinavsingh762006@gmail.com>
2026-07-28 22:42:44 +0530

    maximumprodct

---
+++ maximumprodOF2element.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int a = nums.size();
+        int ans =(nums[a-1]-1 )* (nums[a-2]-1);
+        
+
+        return ans;
+    }
+};
