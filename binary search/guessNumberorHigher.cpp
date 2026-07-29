ea96ca8824afcf292227ff8f60b0e443d259f474
abhinav singh <abhinavsingh762006@gmail.com>
2026-07-29 10:30:00 +0530

    Your commit message

---
+++ binary search/guessNumberorHigher.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int guess ( int n ){
+    int srand = time(0);
+
+    int a  =  rand();
+    return a;
+}
+
+class Solution {
+public:
+   
+    int guessNumber(int n) {
+        int start = 1;
+        int end = n;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int res = guess(mid);
+
+            if (res == 0)
+                return mid;
+            else if (res == -1)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+
+        return -1;
+    }
+};
