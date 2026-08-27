920e550fb24d55dab3226b97ec001ecb831d86d1
abhinav singh <abhinavsingh762006@gmail.com>
2026-08-27 18:30:00 +0530

    removeDuplicate2pointer

--- twoPointers/removeDuplicate.cpp
+++ twoPointers/removeDuplicate.cpp
@@ -2,3 +2,37 @@
 
 using namespace std;
 
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        if (nums.size() == 0) {
+            return 0;
+        }
+
+        int i = 1;
+        int j = 0;
+        int ne = 1;
+        int n = nums.size();
+
+        int arr[n];
+
+        arr[0] = nums[0];
+        while (j < n - 1) {
+
+            if (nums[j] != nums[j + 1]) {
+                arr[i] = nums[j + 1];
+                i++;
+                ne++;
+            }
+
+            j++;
+        }
+
+        for (int k = 0; k < ne; k++) {
+            nums[k] = arr[k];
+        }
+
+        return ne;
+    }
+};
