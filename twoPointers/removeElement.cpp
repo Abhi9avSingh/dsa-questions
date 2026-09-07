de2f1aaeaffbc5b3278cac1e351c448ab2bef09b
abhinav singh <abhinavsingh762006@gmail.com>
2026-09-07 15:38:42 +0530

    removeelement

--- twoPointers/removeElement.cpp
+++ twoPointers/removeElement.cpp
@@ -1,3 +1,29 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+
+
+class Solution {
+public:
+   int removeElement(vector<int>& nums, int val) {
+
+    vector<int> ans;
+    int n = 0;
+
+     
+    for (int i = 0; i < nums.size(); i++) {
+
+        if (val != nums[i]) {
+            ans.push_back(nums[i]);
+            n++;
+        }
+    }
+
+     
+    for (int i = 0; i < n; i++) {
+        nums[i] = ans[i];
+    }
+
+    return n;
+}
+};
