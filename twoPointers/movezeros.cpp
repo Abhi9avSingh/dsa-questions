17ddb415df11b9f6d4d8fd49546f88c16cc7d1a6
abhinav singh <abhinavsingh762006@gmail.com>
2026-09-14 22:00:00 +0530

    movezeros

--- twoPointers/movezeros.cpp
+++ twoPointers/movezeros.cpp
@@ -2,3 +2,25 @@
 
 using namespace std;
 
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+
+        int count = 0;
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+
+            if (nums[i] == 0) {
+                count++;
+            } else {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        while (j < n) {
+            nums[j] = 0;
+            j++;
+        }
+    }
+};
