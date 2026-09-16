25d89c785528fb2ca922e1003a5a1b8ced48d467
abhinav singh <abhinavsingh762006@gmail.com>
2026-09-16 22:00:00 +0530

    sortArrayByParity

--- twoPointers/sortArrayByParity.cpp
+++ twoPointers/sortArrayByParity.cpp
@@ -3,3 +3,27 @@
 using namespace std;
 
 
+class Solution {
+public:
+   vector<int> sortArrayByParity(vector<int>& nums) {
+    int n = nums.size();
+    int j = n - 1;
+    int k = 0;
+
+    vector<int> ans = nums;
+
+    for (int i = 0; i < nums.size(); i++) {
+
+        if (ans[i] % 2 == 0) {
+            nums[k] = ans[i];
+            k++;
+        }
+        else {
+            nums[j] = ans[i];
+            j--;
+        }
+    }
+
+    return nums;
+}
+};
