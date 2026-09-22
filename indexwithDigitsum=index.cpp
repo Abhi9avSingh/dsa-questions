1dd2c71f9bfc8a59cff74d926e7777c5505604ed
abhinav singh <abhinavsingh762006@gmail.com>
2026-09-22 22:00:00 +0530

    lc3550

--- indexwithDigitsum=index.cpp
+++ indexwithDigitsum=index.cpp
@@ -1,2 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
+class Solution {
+public:
+    int smallestIndex(vector<int>& nums) {
+        int a=0;
+        for ( int i =0;i<nums.size();i++){
+            int sum =0;
+            int n = nums[i];
+            while ( n!=0){
+                sum = sum + n%10;
+                n=n/10;
+            }
+            if ( i == sum){
+                return i;
+                break;
+                a++;
+
+            }
+             
+        }
+        if ( a==0){
+            return -1;
+        }
+        return {};
+    
+    }
+};
