902e60f7038bb90a429208bb246b8ef889978e0e
abhinav singh <abhinavsingh762006@gmail.com>
2026-09-24 16:39:22 +0530

    validAnagram

--- strings/validAnagram.cpp
+++ strings/validAnagram.cpp
@@ -2,3 +2,26 @@
 
 using namespace std;
 
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        int i = 0;
+        while (i < s.size()) {
+            if (s[i] != t[i]) {
+                return false;
+                break;
+            }
+            i++;
+           
+        }
+        return true;
+    }
+};
+
+
+/////////////// most optimized approach is frequency which i didnt study yet //
