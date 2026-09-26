c182b79588ec0e4d1ba6cbc7c99fa092df30dd06
abhinav singh <abhinavsingh762006@gmail.com>
2026-09-26 16:36:22 +0530

    vector

--- stl/vector.cpp
+++ stl/vector.cpp
@@ -5,6 +5,19 @@ using namespace std;
 int main ( ){
 
     vector <int>v;
+
+
+    vector<int>a(5,1);
+
+    cout<<"print a"<<endl;
+    for ( int i:a){
+        cout<<i<<"";
+    }
+
     v.push_back(1);
+    cout<<"front"<<v.front()<<endl;
+    cout<<"back"<<v.back()<<endl;
+
+
     
 }
