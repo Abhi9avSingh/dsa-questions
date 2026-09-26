0b4e19e1fa47e2024230ce153f5e12aa5f2a3568
abhinav singh <abhinavsingh762006@gmail.com>
2026-09-26 17:29:52 +0530

    binarySearch

---
+++ stl/algos.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+    vector<int>v = { 1,2,3,4,5};
+    // binary search for finding an element
+
+    binary_search(v.begin (),v.end(),5);
+    
+    
+
+}
