674f1e77c74741bbe98141b5785305b2b199da3e
abhinav singh <abhinavsingh762006@gmail.com>
2026-09-26 17:05:10 +0530

    queue

---
+++ stl/queue.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main ( ){
+//first int first out 
+//creating a stack
+
+queue<string>q;
+
+q.push("love");
+q.push("babbar");
+q.push("some");
+
+q.pop();//top element will be deleted
+q.front();//first elemnt
+q.size();//size of stack
+q.empty();//to delte elements
+
+
+}
