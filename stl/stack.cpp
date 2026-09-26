e5437c1eea64bb01b45e8be733b821c3b07b66a2
abhinav singh <abhinavsingh762006@gmail.com>
2026-09-26 17:01:46 +0530

    stack

--- stl/stack.cpp
+++ stl/stack.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 int main ( ){
 //last int first out 
+//creating a stack
 
+stack<string>s;
+
+s.push("love");
+s.push("babbar");
+s.push("some");
+
+s.pop();
+s.top();//top elemnt
+s.size();//size of stack
+s.empty();//to delte elements
 
 
 }
